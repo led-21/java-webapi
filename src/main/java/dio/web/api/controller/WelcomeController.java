f8d158beb2734ad43ffc8097a2846bb589122813
adriano <adriano.godoy@gmail.com>
2024-03-06 20:00:36 -0400

    First Commit

---
+++ src/main/java/dio/web/api/controller/WelcomeController.java
@@ -0,0 +1,15 @@
+package dio.web.api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping
+    public String welcome(){
+        return "Bem vindo ao meu Spring Boot Web API";
+    }
+}
