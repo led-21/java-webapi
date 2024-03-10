e09fd2c272f058d635e3cb082a82e5c65ccd1359
adriano <adriano.godoy@gmail.com>
2024-03-09 21:36:56 -0400

    First Commit

--- src/main/java/dio/web/api/controller/UsuarioController.java
+++ src/main/java/dio/web/api/controller/UsuarioController.java
@@ -3,6 +3,7 @@ package dio.web.api.controller;
 import dio.web.api.model.Usuario;
 import dio.web.api.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
